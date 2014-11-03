9f7945f47af9050b8d4cddcf849b868a073b4e42
kostaNew <kostaNew@gmail.com>
2014-11-03 07:29:14 +0600

    Get-NetFramework.ps1 - function, which get all installed version of .NET framework

---
+++ Get-NetFramework.ps1
@@ -0,0 +1,12 @@
+﻿function Get-NetFramework(){
+	
+	$allVersion = @()
+
+	#This code is get versions of all .NET frameworks from 2.0 and above, which have installed in Windows 
+	$frameworkKeyRoot = "HKLM:\Software\Microsoft\NET Framework Setup\NDP"
+	$subroutes = Get-ChildItem $frameworkKeyRoot -Recurse
+	$allVersion += ($subroutes | ForEach {Get-ItemProperty $_.PSPath} | Where {($_.Install -eq 1) -or ($_.SuccessInstall -eq 1)}).Version
+
+	$installedFrameworks += $allVersion | Sort -Unique
+	return $installedFrameworks
+}
